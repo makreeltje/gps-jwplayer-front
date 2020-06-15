77661264c52c23a4b2a8c1554deb07bac7600ed4
Rick Meels <rick.meels@gmail.com>
2020-06-15 10:19:03 +0200

    Reconfigured nginx.conf

--- tests/e2e/specs/edit/selectCaption.js
+++ tests/e2e/specs/edit/selectCaption.js
@@ -21,7 +21,7 @@ module.exports = {
             .assert.containsText('//*[@id="app"]/div[1]/div[2]/main/div/div/div[1]/div/h1', 'Dont Jump Edward')
             .click('//*[@id="app"]/div[1]/div[2]/main/div/div/div[1]/div/div/div[1]/div[1]')
             .pause(250)
-            .click('//*[@id="list-item-83-0"]')
+            .click('/html/body/div/div[2]/div/div[1]')
             .assert.visible('//*[@id="app"]/div[1]/div[2]/main/div/div/div[1]/div/div[2]')
             .assert.containsText('//*[@id="app"]/div[1]/div[2]/main/div/div/div[1]/div/div[2]', 'Caption loaded successfully')
             .end()
